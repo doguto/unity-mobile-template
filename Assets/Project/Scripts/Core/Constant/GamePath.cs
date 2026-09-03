5d0add17f5a036d239827edc1c2c3929439a69d6
kuto <doguto110@gmail.com>
2026-09-03 17:49:38 +0900

    Chore: DataStoreのTexturesの作成

--- Assets/Project/Scripts/Core/Constant/GamePath.cs
+++ Assets/Project/Scripts/Core/Constant/GamePath.cs
@@ -3,6 +3,8 @@ namespace Project.Scripts.Core.Constant
     public static class GamePath
     {
         public const string Root = "Assets/Project";
+        public const string DataStore = Root + "/DataStore";
+        public const string Textures = Root + "/Textures";
         public const string Scenes = Root + "/Scenes";
     }
 }
