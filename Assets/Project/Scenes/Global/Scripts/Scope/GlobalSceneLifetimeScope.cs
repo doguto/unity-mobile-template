29bbed0cff65396e6cf140f5a00d3306ad618ee0
kuto <doguto110@gmail.com>
2026-09-03 18:03:51 +0900

    Chore: Globalシーンの作成

---
+++ Assets/Project/Scenes/Global/Scripts/Scope/GlobalSceneLifetimeScope.cs
@@ -0,0 +1,9 @@
+using VContainer;
+using VContainer.Unity;
+
+public class GlobalSceneLifetimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+    }
+}
